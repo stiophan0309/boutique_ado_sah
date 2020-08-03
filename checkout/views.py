d11c738eb6da432f706aac8e61684f4ba835b744
Stìophan A. Horne <steven.a.horne@btinternet.com>
2020-08-03 19:47:02 +0000

    Added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_I7LqZolSHf5IbR2yVu6hjsNN00SuQ4nmJK',
+        'client_secret': 'test client secret'
     }
 
     return render(request, template, context)
