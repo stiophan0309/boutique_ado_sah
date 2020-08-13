644b27d30631f589359a26b4fb7cdddc7197fe0f
Stìophan A. Horne <steven.a.horne@btinternet.com>
2020-08-13 19:50:28 +0000

    Attempting deployment

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -169,6 +169,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_TMP = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
