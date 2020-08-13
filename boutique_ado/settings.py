6e3f3945f7fc44e28290d1697d8acc199aca0780
Stìophan A. Horne <steven.a.horne@btinternet.com>
2020-08-13 20:13:11 +0000

    Added caching to settings.py

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -169,13 +169,18 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_TMP = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+    
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'boutique-ado-sah'
     AWS_S3_REGION_NAME = 'eu-west-2'
